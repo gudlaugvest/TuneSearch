85824d8a993e1b23a2531cc9e4cc2808ebc9973e
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2023-12-19 13:58:29 +0000

    hehhehhe

--- web/modules/custom/music_search/src/Form/MusicSearchConfigurationForm.php
+++ web/modules/custom/music_search/src/Form/MusicSearchConfigurationForm.php
@@ -7,8 +7,6 @@ use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\spotify_api\SpotifyApiService;
 
-use Drupal;
-
 class MusicSearchConfigurationForm extends ConfigFormBase {
 
   /**
@@ -21,13 +19,10 @@ class MusicSearchConfigurationForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-
-
   public static function create(ContainerInterface $container): ConfigFormBase {
     $instance = parent::create($container);
     $instance->spotifyApiService = $container->get('spotify_api.service');
     return $instance;
-    ;
   }
 
   /**
@@ -49,44 +44,97 @@ class MusicSearchConfigurationForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('music_search.custom_music_search');
-    if ($form_state->has('cpage') && $form_state->get('cpage') == 2) {
-      return $this->buildSpotifyPage($form, $form_state);
+    $page = $form_state->get('cpage') ?? 1;
+
+    if ($page === 1) {
+      // Search by Artist
+      $form['search_artist'] = [
+        '#type' => 'textfield',
+        '#title' => t('Search for an Artist'),
+        '#description' => t('Please provide the name of an artist'),
+        '#required' => TRUE,
+      ];
+
+      // Search by Album
+      $form['search_album'] = [
+        '#type' => 'textfield',
+        '#title' => t('Search for an Album'),
+        '#description' => t('Please provide the title of an album'),
+        '#required' => TRUE,
+      ];
+
+      $form['next'] = [
+        '#type' => 'submit',
+        '#value' => 'Next',
+        '#submit' => ['::submitForm'],
+      ];
+    } elseif ($page === 2) {
+      // Display the entered search data
+      $form['search_artist'] = [
+        '#type' => 'textfield',
+        '#title' => t('Search for an Artist'),
+        '#default_value' => $form_state->get('search_data')['search_artist'],
+        '#disabled' => TRUE,
+      ];
+
+      $form['search_album'] = [
+        '#type' => 'textfield',
+        '#title' => t('Search for an Album'),
+        '#default_value' => $form_state->get('search_data')['search_album'],
+        '#disabled' => TRUE,
+      ];
+
+      // Display Spotify data
+      $form += $this->buildSpotifyPage($form, $form_state);
     }
 
-    // Search by Artist
-    $form['search_artist'] = [
-      '#type' => 'textfield',
-      '#title' => t('Search for an Artist'),
-      '#description' => t('Please provide the name of an artist'),
-      '#required' => TRUE,
-    ];
-
-    // Search by Album
-    $form['search_album'] = [
-      '#type' => 'textfield',
-      '#title' => t('Search for an Album'),
-      '#description' => t('Please provide the title of an album'),
-      '#required' => TRUE,
-    ];
-
-    $form['next'] = [
-      '#type' => 'submit',
-      '#value' => 'Next',
-      '#submit' => ['::submitForm', '::spotifyPage'],
-    ];
-
     return parent::buildForm($form, $form_state);
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $page = $form_state->get('cpage') ?? 1;
+
+    if ($page === 1) {
+      // Get the search data from the first page
+      $searchData = $form_state->getValues();
+  
+      // Make a call to the Spotify API and retrieve data
+      $accessToken = $this->spotifyApiService->getToken();
+      $artistData = $this->spotifyApiService->getArtistInfo($searchData['search_artist'], $accessToken);
+      $albumData = $this->spotifyApiService->getAlbumInfo($searchData['search_album'], $accessToken);
+  
+      // Set the retrieved data and the entered search data in the form state
+      $form_state->set('artist_data', $artistData);
+      $form_state->set('album_data', $albumData);
+      $form_state->set('search_data', $searchData);
+    } elseif ($page === 2) {
+      // Save the data from the second page
+      $searchData = $form_state->get('search_data');
+      $selectedFields = $form_state->getValue('save_fields');
+
+      // Example: Save selected fields to the database (adjust as needed)
+      foreach ($selectedFields as $fieldName => $value) {
+        if ($value) {
+          $this->config('music_search.custom_music_search')
+            ->set($fieldName, $searchData[$fieldName])
+            ->save();
+        }
+      }
+    }
+
+    // Redirect to the next page or stay on the same page
+    $form_state->set('cpage', $page + 1);
+    $form_state->setRebuild(true);
   }
 
   /**
    * Build the Spotify page.
    */
   public function buildSpotifyPage(array $form, FormStateInterface $form_state) {
-    // ...
-
-    // Get the artist info and album info from the form state
+    // Get the artist info, album info, and search data from the form state
     $artistData = $form_state->get('artist_data');
     $albumData = $form_state->get('album_data');
+    $searchData = $form_state->get('search_data');
 
     // Display the artist info
     $form['artist_info'] = [
@@ -142,60 +190,17 @@ class MusicSearchConfigurationForm extends ConfigFormBase {
       '#options' => [
         'artist_name' => t('Artist Name'),
         'artist_image' => t('Artist Image'),
+        'album_name' => t('Album Name'),
+        'album_image' => t('Album Image'),
+        'album_popularity' => t('Album Popularity'),
       ],
     ];
 
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => t('Save to Database'),
-      '#submit' => ['::submitForm'],
     ];
 
     return $form;
   }
-
-
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    // Save the data from the first page
-    $searchData = $form_state->get('data');
-    $selectedFields = $form_state->getValue('save_fields');
-
-    // Example: Save selected fields to the database (adjust as needed)
-    foreach ($selectedFields as $fieldName => $value) {
-      if ($value) {
-        $this->config('music_search.custom_music_search')
-          ->set($fieldName, $searchData[$fieldName])
-          ->save();
-      }
-    }
-
-    // Redirect to the first page after saving data
-    $form_state->setRedirect('music_search.spotify_page');
-}
-
-  /**
-   * Submit handler for the "Next" button.
-   */
-  public function spotifyPage(array &$form, FormStateInterface $form_state) {
-    // Get the search data from the first page
-    $searchData = $form_state->getValues();
-    
-    // Set the data in the form state
-    $form_state->set('data', $searchData);
-
-    // Make a call to the Spotify API and retrieve data
-    $accessToken = $this->spotifyApiService->getToken();
-    $artistData = $this->spotifyApiService->getArtistInfo($searchData['search_artist'], $accessToken);
-    $albumData = $this->spotifyApiService->getAlbumInfo($searchData['search_album'], $accessToken);
-
-    // Set the retrieved data in the form state
-    $form_state->set('artist_data', $artistData);
-    $form_state->set('album_data', $albumData);
-
-    // Move to the second page
-    $form_state->set('cpage', 2);
-    $form_state->setRebuild(true);
-  }
-
-
 }
